84ac1dde4861ce7f2d31152fbd7fb321dcce16f8
Chub <ychubykin@gmail.com>
2022-11-22 18:06:14 +0300

    Сделал окончательные версии ДЗ в 1.0

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,11 +6,12 @@ int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число 2, не равное числу 1: ");
 int m = Convert.ToInt32(Console.ReadLine());
 
-// while (n = m)
-// {
-//     Console.Write("Вы ошиблись!\nВведите разные числа: ");
-//     n = Convert.ToInt32(Console.ReadLine());
-// }
+while (n == m)
+{
+    Console.Write("Вы ошиблись!\nВведите другое число 2: ");
+    n = Convert.ToInt32(Console.ReadLine());
+
+}
 
 if (n > m)
     Console.Write(n);
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -9,11 +9,16 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число 3, не равное числам 1 и 2: ");
 int l = Convert.ToInt32(Console.ReadLine());
 
-// while (n = m)
-// {
-//     Console.Write("Вы ошиблись!\nВведите разные числа: ");
-//     n = Convert.ToInt32(Console.ReadLine());
-// }
+while (n == m || n == l || m == l)
+{
+    Console.WriteLine("Вы ошиблись!\nВведите разные числа: ");
+    Console.Write("Первое: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Второе: ");
+    m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Третье: ");
+    l = Convert.ToInt32(Console.ReadLine());
+}
 
 if (n > m && n > l)
     Console.Write(n);
