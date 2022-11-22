58ee9cd158d8d313892d5d639f7ae80caa66458c
Chub <ychubykin@gmail.com>
2022-11-22 18:29:59 +0300

    Сделал итоговые версии ДЗ

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Ввести числo, вывести, делится ли без остатка на 2.
 Console.Clear();
-Console.Write("Введите число 1: ");
+Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 if (n % 2 == 0)
-    Console.Write("Это число делится на 2 без остатка");
+    Console.Write("Это число ЧЕТНОЕ (делится на 2 без остатка)");
 else
-    Console.Write("Это число НЕ делится на 2 без остатка");
+    Console.Write("Это число НЕЧЕТНОЕ (НЕ делится на 2 без остатка)");
